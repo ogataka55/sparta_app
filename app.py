354b557918103094c7fc352eb7ea6b9cbbcc86de
ogataka55 <ogawatakahiro2107@yahoo.co.jp>
2022-02-09 22:27:30 +0900

    third commit(02/09水)

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
 
-import db
-
 app = Flask(__name__)
 
 
-@app.route("/index")
+@app.route("/")
 def index():
-    customers = db.get_all_customers()
+    return render_template("index.html")
 
-    return render_template("index.html", customers=customers)
+
+# @app.route("/index")
+# def index():
+#     customers = db.get_all_customers()
+#
+#     return render_template("index.html", customers=customers)
 
 
 if __name__ == '__main__':
